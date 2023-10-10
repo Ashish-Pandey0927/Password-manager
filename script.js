5ad1beeb8a50f9e785495fd2777e420042c6d176
Ashish Pandey <126936221+Ashish-Pandey0927@users.noreply.github.com>
2023-10-10 18:56:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function myfunction(){
+    let x = document.querySelector(".wrapper");
+    x.style.display = "block";
+}
+function close_func(){
+    let close = document.querySelector(".wrapper");
+    close.style.display = "none";
+
+}
