f4aa1e84c0b0d5422d8ef6781e905ac794fa1a78
Ashish Pandey <126936221+Ashish-Pandey0927@users.noreply.github.com>
2023-10-10 19:39:43 +0530

    Add Nav_bar hover effect

--- script.js
+++ script.js
@@ -7,3 +7,30 @@ function close_func(){
     close.style.display = "none";
 
 }
+
+// +++++++++++++++++++++++navbar active+++++++++++++++
+var btnContainer = document.getElementById("header");
+var btns = btnContainer.getElementsByClassName("nav_a");
+
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+      var current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    });
+  }
+
+
+// for (var i = 0; i < btns.length; i++) {
+//     btns[i].addEventListener("click", function() {
+//       var current = document.getElementsByClassName("active");
+  
+//       // If there's no active class
+//       if (current.length > 0) {
+//         current[0].className = current[0].className.replace(" active", "");
+//       }
+  
+//       // Add the active class to the current/clicked button
+//       this.className += " active";
+//     });
+//   }
